8a38bc3416b66cb43c2dbcee7820aacc26e74e52
Edward Turner <eddturner@ebi.ac.uk>
2014-04-22 18:07:33 +0100

    initial commit of script to fetch poms

---
+++ scripts/get_poms.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+
+repos="interfaces storage"
+currentDir=$(pwd)
+pomDir="$currentDir/../poms";
+if [! -d $pomDir ]; then
+	echo "creating pom directory: $pomDir";
+	mkdir $pomDir;
+fi
+
+echo "pom directory: $pomDir"
+cd $pomDir
+for repo in $repos; do
+	echo "Fetching poms for $repo"
+	for pomInfo in `curl --user dev:uniprot http://wwwdev.ebi.ac.uk/uniprot/artifactory/api/search/gavc?a=$repo\&repos=release | grep ".pom" | sed -e 's/.*\(http.*\.pom\).*/\1/'`; do
+		pomURL=`curl --user dev:uniprot $pomInfo  | grep downloadUri | sed -e 's/.*\(http.*\.pom\).*/\1/'`
+		echo "downloading $pomURL"
+		curl --user dev:uniprot -O $pomURL
+	done
+done
+
+ls -l | grep ".*\.pom"
+if [ $? -ne 0 ]; then
+	echo "no poms downloaded -- exiting";
+	exit 0;
+fi
+pomCount=`ls -l | grep ".*\.pom" | wc -l`
+echo "downloaded $pomCount poms"
+
+cd $currentDir
+
+

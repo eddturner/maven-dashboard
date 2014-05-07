e6eec7fcb19609312e819e52e6770fb576475621
Edward Turner <eddturner@ebi.ac.uk>
2014-05-07 09:48:32 +0100

    show tree graph of dependencies

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,12 +9,13 @@ directivesModule.directive('pomGraph', function ($timeout, DataSource) {
     var linkFn;
     console.log("pom graph 1");
     linkFn = function (scope, element, attrs) {
-        scope.$watch('dependencies', function () {
+        scope.$watch('currentPomName', function () {
             var updateGraph = $timeout(function () {
 
                 console.log("pom graph 2");
-                var pomGraphFile = "../../poms/effective/storage-2014.05.pom.graphml";
-                
+                var pomFile = "../../poms/effective/" + scope.currentPomName;
+                var pomGraphFile = pomFile + ".graphml";
+
                 var xmlTransform = function (data) {
                     console.log("transform data");
                     var x2js = new X2JS();
@@ -30,15 +31,22 @@ directivesModule.directive('pomGraph', function ($timeout, DataSource) {
                     var g = new dagreD3.Digraph();
                     data.node.forEach(function (node) {
                         var nodeId = node._id;
-                        console.log(nodeId);
-                        console.log(node);
-                        g.addNode(nodeId, { label: node.data.ShapeNode.NodeLabel.__text});
+//                        console.log(nodeId);
+//                        console.log(node);
+                        var text = node.data.ShapeNode.NodeLabel.__text;
+                        if(text.indexOf('uk.ac.ebi.uniprot') > -1) {
+                            g.addNode(nodeId, { label: node.data.ShapeNode.NodeLabel.__text, style : "fill: #c5e5c0" });
+                        }
+                        else {
+                            g.addNode(nodeId, { label: node.data.ShapeNode.NodeLabel.__text});
+                        }
                     });
-                    // data.edge.forEach(function(edge){
-                    //     var edgeId = node.id;
-                    //     g.addNode(nodeId, { label: node.y:NodeLabel});
-                    //     g.addEdge(null, mainNodeText, depNodeId);
-                    // });
+                    data.edge.forEach(function (edge) {
+//                        console.log("edge");
+//                        console.log(edge);
+//                         g.addEdge(null, edge._source, edge._target,{ label: edge.data.PolyLineEdge.EdgeLabel.__text});
+                         g.addEdge(null, edge._source, edge._target);
+                    });
 
                     var renderer = new dagreD3.Renderer();
                     var oldDrawNodes = renderer.drawNodes();
@@ -54,44 +62,13 @@ directivesModule.directive('pomGraph', function ($timeout, DataSource) {
                     });
                     console.log(d3.select("svg g"));
                     var layout = dagreD3.layout()
-                        .nodeSep(30)
+                        .nodeSep(20)
                         .rankDir("LR");
                     var layout = renderer.layout(layout).run(g, d3.select("svg g"));
+                    d3.select("svg")
+                        .attr("width", layout.graph().width + 40)
+                        .attr("height", layout.graph().height + 40);
                 }, xmlTransform);
-
-
-
-                var g = new dagreD3.Digraph();
-
-                var mainNodeText = "storage";
-                g.addNode(mainNodeText, { label: mainNodeText, style: "fill: #8ece86" });
-                scope.dependencies.forEach(function (dep) {
-                    if (dep.groupId === 'uk.ac.ebi.uniprot') {
-                        var depNodeId = dep.artifactId + ":" + dep.version;
-                        g.addNode(depNodeId, { label: dep.artifactId + ":" + dep.version});
-                        g.addEdge(null, mainNodeText, depNodeId);
-                    }
-                });
-
-                var renderer = new dagreD3.Renderer();
-                var oldDrawNodes = renderer.drawNodes();
-                renderer.drawNodes(function (graph, root) {
-                    var svgNodes = oldDrawNodes(graph, root);
-                    svgNodes.attr("id", function (u) {
-                        return "node-" + u;
-                    });
-                    svgNodes.select("rect").attr("style", function (u) {
-                        return g.node(u).style;
-                    });
-                    return svgNodes;
-                });
-                console.log(d3.select("svg g"));
-                var layout = dagreD3.layout()
-                    .nodeSep(30)
-                    .rankDir("LR");
-                var layout = renderer.layout(layout).run(g, d3.select("svg g"));
-                console.log("depList");
-                console.log(scope.dependencies);
             });
         });
         console.log("pom graph 3");
@@ -99,7 +76,7 @@ directivesModule.directive('pomGraph', function ($timeout, DataSource) {
 
     return {
         restrict: 'A',
-        scope: { dependencies: '=' },
+        scope: { currentPomName: '=' },
         link: linkFn
     }
 }).directive('pomView', function ($timeout, $compile, DataSource) {
@@ -107,30 +84,30 @@ directivesModule.directive('pomGraph', function ($timeout, DataSource) {
     linkFn = function (scope, element, attrs) {
         scope.$watch('currentPomName', function () {
 //            $timeout(function () {
-                var pomFile = "";
-                console.log("currentpom in directive");
-                console.log(scope.currentPomName);
-                if (attrs.pom === "effective") {
-                    pomFile = "../../poms/effective/" + scope.currentPomName;
-                }
-                else if (attrs.pom === "original") {
-                    pomFile = "../../poms/" + scope.currentPomName;
-                }
-                console.log("pomFile in directive = " + pomFile);
+            var pomFile = "";
+            console.log("currentpom in directive");
+            console.log(scope.currentPomName);
+            if (attrs.pom === "effective") {
+                pomFile = "../../poms/effective/" + scope.currentPomName;
+            }
+            else if (attrs.pom === "original") {
+                pomFile = "../../poms/" + scope.currentPomName;
+            }
+            console.log("pomFile in directive = " + pomFile);
 
-                DataSource.applyTransformation(pomFile, function (data) {
-                    var spaced = vkbeautify.xml(data, 2);
-                    spaced = spaced.replace(/\&/g, '&amp;').
-                        replace(/\</g, '&lt;').
-                        replace(/\>/g, '&gt;').
-                        replace(/"/g, '&quot;');
+            DataSource.applyTransformation(pomFile, function (data) {
+                var spaced = vkbeautify.xml(data, 2);
+                spaced = spaced.replace(/\&/g, '&amp;').
+                    replace(/\</g, '&lt;').
+                    replace(/\>/g, '&gt;').
+                    replace(/"/g, '&quot;');
 
-                    var newContents = angular.element(prettyPrintOne(spaced));
-                    element.html(newContents);
-                    // console.log("replacing element: ");
-                    // console.log(element);
-                    // console.log("with: ");
-                    // console.log(newContents);
+                var newContents = angular.element(prettyPrintOne(spaced));
+                element.html(newContents);
+                // console.log("replacing element: ");
+                // console.log(element);
+                // console.log("with: ");
+                // console.log(newContents);
             });
         }, true)
     };

fcf9c998bd6b974c614cd75cbd010315202fee82
Edward Turner <eddturner@gmail.com>
2014-05-10 17:18:51 +0100

    renamed

---
+++ scripts/pom2effectiveAndGraph.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 POM_FILE";
+fi
+
+currentDir=$( cd "$(dirname "$0")" ; pwd -P );
+poms=$@;
+
+pomDir="$currentDir/../poms";
+effectivePomDir="$currentDir/../poms/effective";
+if [ ! -d $pomDir ]; then
+	echo "creating pom directory: $pomDir";
+	mkdir $pomDir;
+fi
+echo "pom directory: $pomDir"
+
+for pom in $poms; do
+    echo "creating effective pom for, $pom";
+    pomBase=$( basename $pom );
+	(mvn -f $pom org.apache.maven.plugins:maven-dependency-plugin:2.4:tree -DoutputType="graphml" -DoutputFile=$effectivePomDir/$pomBase.graphml) &
+	(mvn -f $pom help:effective-pom -Doutput=$effectivePomDir/$pomBase;
+	 sed -i '' -e 's/^<!--.*-->$//' -e '/^[ \t]*$/d' $effectivePomDir/$pomBase;) &
+done
+wait; # wait for the effective poms to be created
+
+#cd $effectivePomDir;
+#for ePom in $poms; do
+#	sed -i '' -e 's/^<!--.*-->$//' -e '/^[ \t]*$/d' $ePom &
+#done
+#wait; # wait for effective poms to be cleaned up a little
+#cd ..;
+
+echo "created effective poms"
+
